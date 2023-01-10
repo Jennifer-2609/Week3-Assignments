4464189ab966fab9b10c04884bf452f473f82ab8
Jennifer-2609 <121357062+Jennifer-2609@users.noreply.github.com>
2023-01-10 17:03:12 +0530

    Add files via upload

---
+++ system/Computer.java
@@ -0,0 +1,21 @@
+package org.system;
+
+public class Computer {
+
+	public void computerModel() {
+	System.out.println("I have latest computer model");
+	}
+}
+
+
+/*
+Package   :org.system
+Class        :Computer
+Methods   :computerModel()
+
+Class        :Desktop
+Methods   :desktopSize()
+
+Description:
+create above 2 class and call all your class methods into the Desktop using single inheritance.
+*/
